ce1b867d26ffb4b55cbdd0434265a8dac263150d
paromeosi <qualcosacosigallery@gmail.com>
2025-05-22 13:04:23 +0200

    fix: correggi JSX in _app.js per layout full-screen

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+// src/pages/_app.js
+import '../styles/globals.css'
+import Nav from '../components/Nav'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div className="flex flex-col h-screen">
+      <Nav />
+      <main className="flex-1 overflow-hidden">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
