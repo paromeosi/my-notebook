1126c43c1dd8f90cab9b55f9244b640a98be659b
paromeosi <qualcosacosigallery@gmail.com>
2025-05-22 13:43:29 +0200

    chore: final push of all changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,18 @@
 // src/components/Nav.js
-import Link from 'next/link';
+import Link from 'next/link'
 
 export default function Nav() {
   return (
-    <nav className="bg-white shadow p-4 flex space-x-6">
-      <Link href="/" className="hover:text-indigo-600">
-        ✏️ Scrivi
-      </Link>
-      <Link href="/archive" className="hover:text-indigo-600">
-        📚 Archivi
-      </Link>
-      <Link href="/bio" className="hover:text-indigo-600">
-        🔗 Bio & Link
-      </Link>
+    <nav className="bg-white shadow p-4 flex items-center justify-between">
+      <div className="flex space-x-6">
+        <Link href="/" className="hover:text-indigo-600">✏️ Scrivi</Link>
+        {/* <Link href="/archive" className="hover:text-indigo-600">📚 Archivi</Link> */}
+        <Link href="/bio" className="hover:text-indigo-600">🔗 Bio & Link</Link>
+      </div>
+      {/* Banner di annuncio */}
+      <div className="bg-indigo-100 text-indigo-800 px-3 py-1 rounded">
+        📸 Take a screenshot!
+      </div>
     </nav>
-  );
+)
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// src/pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="it">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* altri meta se vuoi */}
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
